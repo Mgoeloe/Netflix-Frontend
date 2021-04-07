221327d89fe2f6f8613fa347f8d2794e4dde3268
Yassin <y.chakai@icloud.com>
2021-04-07 15:13:59 +0200

    footer
    moet nog gestyled worden

---
+++ netflix/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
