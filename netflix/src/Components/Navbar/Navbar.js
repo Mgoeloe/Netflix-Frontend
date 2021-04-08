282ad4d28847d634339855708f84f01ebf731d06
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-04-08 10:28:06 +0200

    Added more icons

--- netflix/src/Components/Navbar/Navbar.js
+++ netflix/src/Components/Navbar/Navbar.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './Nav.css';
+
 import Logo from '../../IMG/netflix-82871.png';
+import Avatar from '../../IMG/netflix-avatar.png';
+
 import SearchIcon from '@material-ui/icons/Search';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 const Navbar = () => {
     return (
@@ -18,10 +23,12 @@ const Navbar = () => {
                     <a href=' ' ><SearchIcon /></a>
                     <a href=' ' >Kids </a>
                     <a href=' ' >DVD </a>
-                    <a href=' ' >? </a>
-                    <a href=' ' >? </a>
+                    <a href=' ' ><CardGiftcardIcon /></a>
+                    <a href=' ' ><NotificationsIcon /></a>
+                    <a href=' ' ><img src={Avatar} alt='Avatar' /></a>
+                    <a href=' ' id='Triangle'> &#9660;</a>
                 </div>
-                
+
             </div>
         </div>
     )
