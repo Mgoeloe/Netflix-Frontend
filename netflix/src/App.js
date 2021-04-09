2a86dd6b21c83e3529141d1650389dbec77e17bb
Boeruchan <74913804+Boeruchan@users.noreply.github.com>
2021-04-09 09:57:15 +0200

    Banner aangemaakt

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
+import Banner from './Components/Banner/Banner';
 
 const App = () => {
   return (
     <div>
       {/* nav */}
       <Navbar />
+      <Banner />
 
 
       
