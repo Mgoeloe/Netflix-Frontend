8f449aa7b82291d096c7e83f57148864515820e6
YasChakai <y.chakai@icloud.com>
2021-04-12 09:36:17 +0200

    Footer

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import Homepage from './Components/Homepage/Homepage';
 
+
 const App = () => {
   return (
     <div>
       <Homepage />
-
-    </div>
+      
+            </div>
   )
 }
 
