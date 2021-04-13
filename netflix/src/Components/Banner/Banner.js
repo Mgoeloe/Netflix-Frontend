e2fb20269b260bb2a176da0ebf27e53e83d285f9
Boeruchan <74913804+Boeruchan@users.noreply.github.com>
2021-04-13 17:05:32 +0200

    Styling banner

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -11,19 +11,17 @@ const Banner = () => {
   let urlInfo = "";
 
   return (
-    <div className="BannerPicture">
-      <img src={matrix} alt="bg" className="BannerBg" />
+    <div className="banner">
+      <div className="banner__contents">
       <img src={movielogo} alt="movielogo" className="BannerLogo" />
       <h1>Watch Matrix 4</h1>
-      <div className="BtnBannerWrap">
-        <button onClick={url}>
-          <PlayArrow className="IconBanner" />
-          Play
-        </button>
-        <button onClick={urlInfo} className="BtnInfo">
-          <InfoOutlined className="IconBanner" />
-          More info test
-        </button>
+      <p>When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.</p>
+      <div className="banner__buttons">
+        <button className="banner__button">
+          <PlayArrow className="IconBanner" />Play</button>
+        <button className="banner__button dark">
+          <InfoOutlined className="IconBanner" />More info</button>
+      </div>
       </div>
     </div>
   );
