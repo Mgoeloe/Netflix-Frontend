b49b7b311a893f6cf490eae54339b248d7f28ded
Boeruchan <74913804+Boeruchan@users.noreply.github.com>
2021-04-15 15:31:42 +0200

    v.1 banner done

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -5,6 +5,8 @@ import moviebg from "../../IMG/matrix.jpg";
 import "../../Styles/style.css";
 import PlayArrow from "@material-ui/icons/PlayArrow";
 import InfoOutlined from "@material-ui/icons/InfoOutlined";
+import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
+import SentimentVerySatisfiedTwoToneIcon from '@material-ui/icons/SentimentVerySatisfiedTwoTone';
 
 const Banner = () => {
   const [state, setState] = useState(false);
@@ -15,14 +17,27 @@ const Banner = () => {
     <div className="banner">
       <img src={moviebg} alt="moviebg" className="bg" />
       <div className="banner__contents">
-      <img src={movielogo} alt="movielogo" className="BannerLogo" />
-      <p>When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.</p>
-      <div className="banner__buttons">
-        <button>
-          <PlayArrow className="IconBanner" />Play</button>
-        <button className="dark">
-          <InfoOutlined className="IconBanner" />More info</button>
+        <img src={movielogo} alt="movielogo" className="BannerLogo" />
+        <p>
+          When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he
+          discovers the shocking truth--the life he knows is the elaborate deception of an evil
+          cyber-intelligence.
+        </p>
+        <div className="banner__buttons">
+          <button>
+            <PlayArrow className="IconBanner" />
+            Play
+          </button>
+          <button className="dark">
+            <InfoOutlined className="IconBanner" />
+            More info
+          </button>
+        </div>
       </div>
+      <div className="banner__extra">
+        <ReplayOutlinedIcon className="banner__replay" />
+       <div className="banner__age_bg"> <SentimentVerySatisfiedTwoToneIcon className="banner__age_icon" />
+       </div>
       </div>
     </div>
   );
