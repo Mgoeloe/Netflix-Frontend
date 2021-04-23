61171f742024d865a98177eb3b69938f2feecb1b
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-04-23 11:56:34 +0200

    Connected discover page endpoint

--- netflix/src/Components/Axios/Axios.js
+++ netflix/src/Components/Axios/Axios.js
@@ -5,22 +5,24 @@ import axios from 'axios';
 const Axios = () => {
     const randomMovie = ~~(1000*Math.random());
 
-    const apiKey = '43adde1f22cb5d9f3d7d5852fa42e5e6';
-    const apiURL = `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=${apiKey}`;
+    // const apiKey = '43adde1f22cb5d9f3d7d5852fa42e5e6';
+    // const apiURL = `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=${apiKey}`;
 
-    const [movieInfo, setMovieInfo] = useState();
+    const [movies, setMovies] = useState();
+
+    const DISCOVER_REST_API_URL = 'http://localhost:8080/api/movies/discover';
     
 
 
     const fetchData = (async () => {
-        const response = await axios.get(apiURL);
+        const response = await axios.get(DISCOVER_REST_API_URL);
 
-        setMovieInfo(response.data);
+        setMovies(response.data);
 
-        console.log(response.data.backdrop_path);
-        // console.log(movieInfo);
+        // console.log(response.data.backdrop_path);
+        // console.log(movies);
 
-        const movieImage = response.data.backdrop_path;
+        // const movieImage = response.data.backdrop_path;
 
     })()
 
@@ -28,9 +30,13 @@ const Axios = () => {
         <div>
             <p>test</p>
             <button onClick={fetchData}>fetch</button>
-            {movieInfo && (
-                <img src={`https://image.tmdb.org/t/p/w500/${movieInfo.backdrop_path}`} alt='Backdrop' />
-            )}
+            <div className='movies'>
+                {movies && movies.map((movie, index) => {
+                    return([])
+                }
+                    // <img src={`https://image.tmdb.org/t/p/w500/${movieInfo.backdrop_path}`} alt='Backdrop' />
+                )}
+            </div>
         </div>
     )
 }
