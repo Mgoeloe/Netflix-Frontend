66394299bf5c1bbc51a1854b27f38f7b6b5bc78b
Boeruchan <74913804+Boeruchan@users.noreply.github.com>
2021-04-29 16:50:22 +0200

    working movieInfo

--- netflix/src/Components/MovieInfo/MovieInfo.js
+++ netflix/src/Components/MovieInfo/MovieInfo.js
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../../Styles/style.css";
-import Axios from '../Axios/Axios';
+import Axios from "../Axios/Axios";
 
 const MovieInfo = () => {
-    const [movieInfo, setMovieInfo] = useState([]);
-    const movieID = '603';
-    const MOVIEINFO_REST_API_URL = `http://localhost:8080/api/movies/${movieID}`;
+  const [movieInfo, setMovieInfo] = useState(null);
+  const movieID = "603";
+  const MOVIEINFO_REST_API_URL = `http://localhost:8080/api/movies/${movieID}`;
 
-    useEffect(() => {
-        const fetchData = (async () => {
-            const response = await axios.get(MOVIEINFO_REST_API_URL);
-    
-            setMovieInfo(response.data);
-    
-            // console.log(response.data.backdrop_path);
-            console.log(movieInfo);
-            console.log(response.data);
-    
-            // const movieImage = response.data.backdrop_path;
-    
-        })()
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(MOVIEINFO_REST_API_URL);
+      setMovieInfo(response.data);
+    };
+    fetchData();
+  }, []);
 
+  //    console.log(movieInfo);
 
-    }, [])
-
-
+  if (movieInfo === null) {
+    return <p>loading</p>;
+  } else {
     return (
-        <div className= 'movieInfo'>
-            <p>{movieInfo.overview}</p>
+      <div className="movieInfo">
+        {movieInfo.genres.map((gen, index) => {
+          return <p key={index}>{gen.name}</p>;
+        })}
+        {/* {console.log(movieInfo)} */}
+        {/* {console.log(movieInfo.genres.name)} */}
 
+        {/* {console.log(movieInfo.genres[1].name)} */}
+        {/* <p>{movieInfo.genres[1].name}</p> */}
+        {/* <p>{movieInfo}</p> */}
+        {/* <p>{movieInfo.overview}</p>
             <p>Cast: </p>
 
             <p>Genres: {movieInfo.tagline}</p>
 
-            {console.log(movieInfo.genres)}
+            {console.log(movieInfo.genres)} */}
+      </div>
+    );
+  }
+};
 
-        
-
-        </div>
-    )
-}
-
-export default MovieInfo
+export default MovieInfo;
