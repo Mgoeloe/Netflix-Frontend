22bf252cd3fa1ff44e7120e1ce7e740e5127a8fc
Marlon Goeloe <74913825+Mgoeloe@users.noreply.github.com>
2021-04-30 11:29:15 +0200

    Added popup on homepage

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import movielogo from "../../IMG/matrixlogo.png";
 import "../../Styles/style.css";
 import PlayArrow from "@material-ui/icons/PlayArrow";
@@ -6,8 +6,15 @@ import InfoOutlined from "@material-ui/icons/InfoOutlined";
 import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
 import SentimentVerySatisfiedTwoToneIcon from '@material-ui/icons/SentimentVerySatisfiedTwoTone';
 import BannerPlayer from "./Banner_player";
+import MovieInfo from '../MovieInfo/MovieInfo';
+
 
 const Banner = () => {
+  const [toggleState, setToggleState] = useState(null);
+
+  const toggle = () => {
+    setToggleState(!toggleState);
+  }
 
   return (
     <div className="banner">
@@ -19,21 +26,35 @@ const Banner = () => {
           discovers the shocking truth--the life he knows is the elaborate deception of an evil
           cyber-intelligence.
         </p>
+
         <div className="banner__buttons">
           <button>
             <PlayArrow className="IconBanner" />
             Play
           </button>
-          <button className="dark">
+
+          <button className="dark" onClick={toggle}>
             <InfoOutlined className="IconBanner" />
             More info
           </button>
+
         </div>
+
+
       </div>
+      <div className='toggle'>
+        {toggleState ? <div>
+          <MovieInfo />  <button className="dark" onClick={toggle}>
+            <InfoOutlined className="ClosePopup" />
+                          Close
+                          </button>
+        </div> : null}
+      </div>
+
       <div className="banner__extra">
         <ReplayOutlinedIcon className="banner__replay" />
-       <div className="banner__age_bg"> <SentimentVerySatisfiedTwoToneIcon className="banner__age_icon" />
-       </div>
+        <div className="banner__age_bg"> <SentimentVerySatisfiedTwoToneIcon className="banner__age_icon" />
+        </div>
       </div>
     </div>
   );
--- netflix/src/Components/MovieInfo/MovieInfo.js
+++ netflix/src/Components/MovieInfo/MovieInfo.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "../../Styles/style.css";
 import Axios from '../Axios/Axios';
+import Banner from '../Banner/Banner';
 
 const MovieInfo = () => {
-    const [movieInfo, setMovieInfo] = useState([]);
+    const [movieInfo, setMovieInfo] = useState(null);
     const movieID = '603';
     const MOVIEINFO_REST_API_URL = `http://localhost:8080/api/movies/${movieID}`;
 
     useEffect(() => {
         const fetchData = (async () => {
             const response = await axios.get(MOVIEINFO_REST_API_URL);
-    
+
             setMovieInfo(response.data);
-    
+
             // console.log(response.data.backdrop_path);
             console.log(movieInfo);
             console.log(response.data);
-    
+
             // const movieImage = response.data.backdrop_path;
-    
+
         })()
 
 
@@ -27,16 +28,24 @@ const MovieInfo = () => {
 
 
     return (
-        <div className= 'movieInfo'>
-            <p>{movieInfo.overview}</p>
+        <div className='movieInfo'>
+            {movieInfo && (
+                <div>
+                    <p>{movieInfo.overview}</p>
 
-            <p>Cast: </p>
+                    <p>Cast: {movieInfo.title}</p>
 
-            <p>Genres: {movieInfo.tagline}</p>
+                    <p>Genres: {movieInfo.genres[1].name}</p>
 
-            {console.log(movieInfo.genres)}
+                    {console.log(movieInfo.genres)}
 
-        
+                    {/* <button className="dark" onClick={toggle}>
+                	<InfoOutlined className="IconBanner" />
+                    More info
+                    </button> */}
+
+                </div>
+            )}
 
         </div>
     )
