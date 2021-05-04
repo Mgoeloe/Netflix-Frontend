7cd87ad9706e2227af112b5f835ebe8a659e6071
Boeruchan <74913804+Boeruchan@users.noreply.github.com>
2021-05-04 10:10:45 +0200

    banner wijziging

--- netflix/src/Components/Banner/Banner.js
+++ netflix/src/Components/Banner/Banner.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
+
 import movielogo from "../../IMG/matrixlogo.png";
 import "../../Styles/style.css";
-import PlayArrow from "@material-ui/icons/PlayArrow";
+import ReactPlayer from "react-player/youtube";
 import InfoOutlined from "@material-ui/icons/InfoOutlined";
 import ReplayOutlinedIcon from '@material-ui/icons/ReplayOutlined';
 import SentimentVerySatisfiedTwoToneIcon from '@material-ui/icons/SentimentVerySatisfiedTwoTone';
 import BannerPlayer from "./Banner_player";
+import PlayArrow from "@material-ui/icons/PlayArrow";
 import MovieInfo from '../MovieInfo/MovieInfo';
 
 
 const Banner = () => {
   const [toggleState, setToggleState] = useState(null);
+  const [active, setActive] = useState(false);
 
   const toggle = () => {
     setToggleState(!toggleState);
@@ -18,17 +21,25 @@ const Banner = () => {
 
   return (
     <div className="banner">
-      <BannerPlayer />
-      <div className="banner__contents">
-        <img src={movielogo} alt="movielogo" className="BannerLogo" />
-        <p>
-          When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he
-          discovers the shocking truth--the life he knows is the elaborate deception of an evil
-          cyber-intelligence.
+     {active ? (<div className="player-wrapper">
+        <ReactPlayer playing={true} className="react-player" url="https://www.youtube.com/watch?v=PkhXLgu-mYM" width="100%" height="100%" />
+        <button onClick={() => setActive(!active)} className="player__button">
+          x
+            </button>
+      </div>) : (
+        <>
+          <BannerPlayer />
+          <div className="banner__contents">
+            <img src={movielogo} alt="movielogo" className="BannerLogo" />
+            <p>
+              When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he
+              discovers the shocking truth--the life he knows is the elaborate deception of an evil
+              cyber-intelligence.
         </p>
 
+
         <div className="banner__buttons">
-          <button>
+          <button onClick={() => setActive(true)}>
             <PlayArrow className="IconBanner" />
             Play
           </button>
@@ -58,8 +69,12 @@ const Banner = () => {
         <div className="banner__age_bg"> <SentimentVerySatisfiedTwoToneIcon className="banner__age_icon" />
         </div>
       </div>
-    </div>
-  );
-};
+      </>
+  )      
+}
+</div>
+  )}
 
 export default Banner;
+
+
